8eaab97bd5021d81ac81d67e02c9c853125dbdcb
Cody Murdock <y4cody@gmail.com>
2023-10-25 15:07:16 -0600

    add /Cody

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/Riley', (req,res) => {res.send("Hello Riley")});
 
+app.get('/Cody', (req,res) => {res.send("Hello Cody")}); 
+
 app.listen(port, ()=>console.log("Listening"));
