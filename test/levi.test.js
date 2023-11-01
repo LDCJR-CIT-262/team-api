933dac6c8518d838113eea0ad5844c0e11d71354
Levi Larson <larsonlevi27@gmail.com>
2023-11-01 15:33:55 -0600

    changed code

--- test/levi.test.js
+++ test/levi.test.js
@@ -5,9 +5,7 @@ it("Should say Levi",async ()=>{
     const response = await fetch('http://localhost:3000/levi');
 
     const name = await response.text();
-    const status = response.status
 
-    expect(status).toBe(200)
-    expect(name).toBe("sean");
+    expect(name).toBe("Hello Levi");
 
 })
