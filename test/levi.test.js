b1a972c618b9fb8bc7530d1707977a8711ab672a
Levi Larson <larsonlevi27@gmail.com>
2023-11-01 15:18:55 -0600

    installed node jest2

---
+++ test/levi.test.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch2");
+
+it("Should get a login token",async ()=>{
+
+    let token = "";//this is an emptry string
+    const options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "userName":"scmurdock@gmail.com",
+            "password":"P@ssw0rd"
+        })
+    }
+
+    const response = await fetch('http://localhost:3000/levi');
+
+    const name = await response.text();
+    expect.(name).toBe("sean");
+
+})
